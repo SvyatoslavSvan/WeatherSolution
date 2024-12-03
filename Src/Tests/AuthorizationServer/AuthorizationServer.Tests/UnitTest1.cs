8010050b75e03187512aeef7ffaff1c5d2bcc27c
SvyatoslavSvanPC <casemiron44@gmail.com>
2024-12-03 12:22:43 +0200

    Unit Tests Added

---
+++ Src/Tests/AuthorizationServer/AuthorizationServer.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AuthorizationServer.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Src/Tests/DataCore/DataCore.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DataCore.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
